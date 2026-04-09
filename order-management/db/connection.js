0d79764d24fbdbb97625f043db2f237ebd85519b
angellabn <angellabn77@gmail.com>
2026-04-09 20:57:51 +0530

    uig

--- order-management/db/connection.js
+++ order-management/db/connection.js
@@ -1,14 +1,36 @@
+// const mysql = require('mysql2/promise');
+// require('dotenv').config();
+
+// const pool = mysql.createPool({
+//   host:     process.env.DB_HOST || 'localhost',
+//   user:     process.env.DB_USER || 'root',
+//   password: process.env.DB_PASS || '',
+//   database: process.env.DB_NAME || 'order_management_db',
+//   waitForConnections: true,
+//   connectionLimit:    10,
+//   queueLimit:         0
+// });
+
+// module.exports = pool;
+
+
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST || 'localhost',
-  user:     process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'order_management_db',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
   waitForConnections: true,
-  connectionLimit:    10,
-  queueLimit:         0
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
